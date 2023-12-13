e5b41c34585af868d4cd3ca8d78546bccafb2721
Nurmix <oscar.nurmi.torslund@gmail.com>
2023-12-13 08:09:25 +0100

    Changes by jonatan

--- Test/src/GameManager.java
+++ Test/src/GameManager.java
@@ -33,3 +33,4 @@ public class GameManager {
         Vehicles = vehicles;
     }
 }
+
--- Test/src/VehicleFactory.java
+++ Test/src/VehicleFactory.java
@@ -2,6 +2,15 @@ public class VehicleFactory {
     public static Volvo240 createVolvo() { return new Volvo240(); }
     public static Saab95 createSaab() { return new Saab95(); }
 
-    public static Scania createScania() { return new Scania(); }
+    private static Scania createScania() { return new Scania(); }
+
+    public static Vehicle createVehicle(String modelName) {
+        return switch (modelName) {
+            case "Saab95" -> createSaab();
+            case "Volvo240" -> createVolvo();
+            case "Scania" -> createScania();
+            default -> createVolvo();
+        };
+    }
 
 }
